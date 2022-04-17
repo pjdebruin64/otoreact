a6f4d63db5dbc7a812cc13bb158c8becc1ba9da0
pjdebruin64 <peter@peterdebruin.net>
2022-04-17 21:47:35 +0200

    .

--- minify.bat
+++ minify.bat
@@ -1,6 +1,8 @@
-"C:\Program Files (x86)\GnuWin32\bin\sed.exe" -b -E -f minify.sed otoreact.js > ..\..\test\otoreact\otoreact.js
-copy OtoReact.d.ts ..\..\test\otoreact\
-copy OtoReact.ts ..\..\test\otoreact\
-copy *.html ..\..\test\otoreact\
-copy *.css  ..\..\test\otoreact\
-copy samples.js ..\..\test\otoreact\
+SET %Dest%=%1
+if %Dest%="" SET %Dest%=..\..\test\otoreact\
+"C:\Program Files (x86)\GnuWin32\bin\sed.exe" -b -E -f minify.sed otoreact.js > %Dest%otoreact.js
+copy OtoReact.d.ts .%Dest%
+copy OtoReact.ts .%Dest%
+copy *.html .%Dest%
+copy *.css  .%Dest%
+copy samples.js .%Dest%
---
+++ publish.bat
@@ -0,0 +1,2 @@
+minify.bat ".\Publish\"
+pause
