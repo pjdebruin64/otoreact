4b2aa993f62d1c62daf25502c5bf49cd6be01dbb
Peter de Bruin-Trautmann <p.bruin@otolift.com>
2023-07-25 11:10:32 +0200

    .

--- publish.bat
+++
@@ -1,9 +0,0 @@
-set Dest=Publish\
-call tsc --project ./tsconfig.json
-pause
-call .\Minify.bat %Dest%
-pause
-del ./otoreact.js
-call tsc --project ./tsconfig.2015.json
-pause
-"C:\Program Files (x86)\GnuWin32\bin\sed.exe" -b -E -f minify.sed OtoReact.js > %Dest%OtoReact_ES2015.js
--- test.js
+++
@@ -1,1 +0,0 @@
-globalThis.ttt = 1;
