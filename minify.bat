0b5ef7a8c84aae31818d12e52b8577c4ace093a2
Peter de Bruin-Trautmann <p.bruin@otolift.com>
2023-10-03 09:03:34 +0200

    .

--- minify.bat
+++ minify.bat
@@ -4,5 +4,5 @@ if "%Dest%"=="" SET Dest=..\..\test\otoreact\
 "C:\Program Files (x86)\GnuWin32\bin\sed.exe" -b -E -f minify.sed OtoReact.js > %Dest%OtoReact.js
 copy OtoReact.d.ts %Dest%
 copy OtoReact.ts %Dest%
-copy *.shtml %Dest%
+copy *.html %Dest%
 copy index_files %Dest%
